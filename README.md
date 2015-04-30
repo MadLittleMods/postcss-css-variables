fc0a8f52a6ce763012cf601a49ac27199da84025
Eric Eastwood <contact@ericeastwood.com>
2015-04-30 09:23:28 -0500

    Update readme, clean up plugin

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 PostCSS plugin to transform [`CSS Custom Properties(CSS variables)`](http://dev.w3.org/csswg/css-variables/) syntax into a static representation. This plugin provides a future-proof way of using most of CSS variables featuers.
 
-CSS variables polyfill/shim.
+CSS variables or CSS Custom Properties polyfill/shim.
 
-We strive for the most complete transformation but we this plugin can't achieve complete parity.
+We strive for the most complete transformation but we no plugin can achieve true complete parity according to the [speficification](http://dev.w3.org/csswg/css-variables/) because of the DOM cascade unknowns.
 
 ## Latest Version: 0.1.0
 ### Changelog
@@ -16,34 +16,189 @@ We strive for the most complete transformation but we this plugin can't achieve 
 var postcss = require('postcss');
 var cssvariables = require('postcss-css-variables');
 
-postcss([
-	cssvariables(/*options*/)
-])
-.process(css, opts);
+var fs = require('fs');
+
+var mycss = fs.readFileSync('input.css', 'utf8');
+
+// Process your CSS with postcss-css-variables
+var output = postcss([
+		cssvariables(/*options*/)
+	])
+	.process(mycss)
+	.css;
+
+console.log(output);
 ```
 
 [*For more general PostCSS usage, see this section*](https://github.com/postcss/postcss#usage)
 
+
 ## At-rule support `@media`, `@support`, etc
 
+You can add rules that declare CSS variables nested inside at-rules. You can even nest the at-rules however deep you want.
+
+The following CSS:
+
 ```
-todo
+:root {
+	--width: 100px;
+}
+
+@media (max-width: 1000px) {
+	:root {
+		--width: 200px;
+	}
+}
+
+.box {
+	width: var(--width);
+}
 ```
 
 will be processed to:
 
 ```
-todo
+.box {
+	width: 100px;
+}
+
+@media (max-width: 1000px) {
+	.box {
+		width: 200px;
+	}
+}
+```
+
+
+## Nested rules
+
+When using this feature, `postcss-css-variables` will output invalid CSS by itself(but you did input invalid CSS anyway). This feature is best paired with [`postcss-nested`](https://github.com/postcss/postcss-nested) in order to properly expand the rules.
+
+```
+var postcss = require('postcss');
+var cssvariables = require('postcss-css-variables');
+var nestedcss = require('postcss-nested');
+
+var fs = require('fs');
+
+var mycss = fs.readFileSync('input.css', 'utf8');
+
+// Process your CSS with postcss-css-variables
+var output = postcss([
+		// Process any CSS variables
+		cssvariables(/*options*/),
+		// After we processed all of the variables, flatten/unnest rules
+		nestedcss
+	])
+	.process(mycss)
+	.css;
+
+console.log(output);
+```
+
+### Simple example
+
+The following CSS:
+
+```
+.box-foo {
+	--some-width: 150px;
+	width: var(--some-width);
+
+	.box-bar {
+		width: var(--some-width);
+	}
+}
+```
+
+will be processed to:
+
+```
+.box-foo {
+	width: 150px;
+
+	.box-bar {
+		width: 150px;
+	}
+}
+```
+
+### Complex example
+
+The following CSS:
+
+```
+:root {
+	--some-width: 150px;
+}
+
+.box-foo {
+	width: var(--some-width);
+
+	.box-bar {
+		width: var(--some-width);
+	}
+}
+
+@media (max-width: 800px) {
+	.box-foo {
+		--some-width: 300px;
+	}
+}
+```
+
+will be processed to:
+
+```
+.box-foo {
+	width: 150px;
+
+	.box-bar {
+		width: 150px;
+	}
+
+	@media (max-width: 800px) {
+		.box-bar {
+			width: 300px;
+		}
+	}
+}
+
+@media (max-width: 800px) {
+	.box-foo {
+		width: 300px;
+	}
+}
 ```
 
 
 
 ## Interoperability
 
-`post-css-variables` is plays really nice with [postcss-nested](https://github.com/postcss/postcss-nested) in order to get a larger subset of CSS variables features.
+`post-css-variables` plays really nice with [`postcss-nested`](https://github.com/postcss/postcss-nested) in order to get a larger subset of CSS variables features. *See [Nested rules, Usage section](#nested-rules)*
+
+If you are using [`postcss-custom-properties`](https://github.com/postcss/postcss-custom-properties) previosly, we have a compatible feature set and more so you can switch over without having to refactor any of your code. You can just start writing the new awesome stuff.
 
 
-## Syntax
+# Why?
+
+This plugin was spawned out of a [discussion on the `cssnext` repo](https://github.com/cssnext/cssnext/issues/99) and a personal need.
+
+There is another similar plugin available, [`postcss-custom-properties`](https://github.com/postcss/postcss-custom-properties), which at the moment, covers less of the [CSS-variable/custom-property draft/spec](http://dev.w3.org/csswg/css-variables/) compared to this plugin. It also happened to not cover the feature I was after and after dealing with it, making an incomplete prototype piggybacking off of it; I decided to write my own starting from scratch. I do use some of the same/similar unit tests because that just makes sense to get proper code coverage.
+
+### Differences from `postcss-custom-properties`
+
+The main features that we`post-css-variables` add/provide are:
+
+ - No limitation on what scope CSS variables can be declared or used (`:root` or wherever)
+ 	 - Proper value substition based on explicit DOM/structure traversal
+ - At-rule support `@media`, `@support`, etc
+ - Nested rules which can be fully deduced with [`postcss-nested`](https://github.com/postcss/postcss-nested).
+
+
+
+
+# Syntax
 
 ### [Defining Custom Properties: the --* family of properties](http://dev.w3.org/csswg/css-variables/#defining-variables)
 
@@ -59,7 +214,7 @@ A custom property is any property whose name starts with two dashes (U+002D HYPH
 
 A custom property can be declared multiple times.
 
-See: [*W3C Draft: CSS Custom Properties for Cascading Variables, section 2*](http://dev.w3.org/csswg/css-variables/#defining-variables) for more information.
+*See: [W3C Draft: CSS Custom Properties for Cascading Variables, section 2](http://dev.w3.org/csswg/css-variables/#defining-variables) for more information.*
 
 
 ### [Using Cascading Variables: the var() notation](http://dev.w3.org/csswg/css-variables/#using-variables)
@@ -73,7 +228,9 @@ See: [*W3C Draft: CSS Custom Properties for Cascading Variables, section 2*](htt
 }
 ```
 
-See: [*W3C Draft: CSS Custom Properties for Cascading Variables, section 3*](http://dev.w3.org/csswg/css-variables/#using-variables) for more information.
+*See: [W3C Draft: CSS Custom Properties for Cascading Variables, section 3](http://dev.w3.org/csswg/css-variables/#using-variables) for more information.*
+
+
 
 
 # Options:
@@ -114,19 +271,20 @@ postcss([
 .process(css, opts);
 ```
 
-# Notes
+
+# Quick Reference/Notes
 
  - This plugin was spawned out of a [discussion on the `cssnext` repo](https://github.com/cssnext/cssnext/issues/99)
  - We provide a larger CSS variable feature subset than [`postcss-custom-properties`](https://github.com/postcss/postcss-custom-properties).
- - Solves these issues:
+ - Related links and issues:
  	 - [var declared in media query should pull in properties that use/reference that var *on `cssnext/cssnext`*](https://github.com/cssnext/cssnext/issues/99)
  	 - [Investigate support for media-query scoped properties *on `postcss/postcss-custom-properties`*](https://github.com/postcss/postcss-custom-properties/issues/9)
  	 - [remove `:root` limitation by injecting rules with new declarations that just contains modified properties. *on `postcss/postcss-custom-properties`*](https://github.com/postcss/postcss-custom-properties/issues/1)
 
 
-# Testing:
+# Testing
 
-We have a suite of [Mocha](http://mochajs.org/) tests. If you see something that doesnt' have coverage, make an issue or pull request.
+We have a suite of [Mocha](http://mochajs.org/) tests. If you see something that doesn't have coverage, make an issue or pull request.
 
 Run once:
 
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// PostCSS CSS Variables (postcss-css-variables)
+// v0.1.0
+//
+// https://github.com/MadLittleMods/postcss-css-variables
+
 // For Debugging
 //var nomo = require('node-monkey').start({port: 50501});
 
@@ -21,9 +26,7 @@ var RE_VAR_FUNC = (/var\((--[^,\s]+?)(?:\s*,\s*(.+))?\)/);
 // Note: Resolving a declaration value without any `var(...)` does not harm the final value. 
 //		This means, feel free to run everything through this function
 var resolveValue = function(decl, map, /*optional*/mimicChildOf) {
-	//console.log('');
-	//console.log(new Error().stack);
-	//console.log('Resolving value:', decl.parent.selector, decl.value);
+	
 	// A list of parents that you want the decl to mimic being a child of as well
 	// Good for consuming other variables declared in different scopes
 	// Coerce into an array
@@ -31,11 +34,6 @@ var resolveValue = function(decl, map, /*optional*/mimicChildOf) {
 	mimicChildOfList = [].concat(decl.parent ? [decl.parent] : []);
 	// Then add on the additional list that was passed in
 	mimicChildOfList = mimicChildOfList.concat(mimicChildOf ?  mimicChildOf : []);
-
-	//console.log('mcflist', mimicChildOfList);
-	//console.log('mcflist', mimicChildOfList.map(function(mimicParentNode) {
-	//	return (mimicParentNode || {}).selector;
-	//}));
 
 	var resultantValue = decl.value;
 	var variablesUsedInValue = [];
@@ -45,9 +43,6 @@ var resolveValue = function(decl, map, /*optional*/mimicChildOf) {
 	var isResultantValueUndefined = false;
 	resultantValue = resultantValue.replace(new RegExp(RE_VAR_FUNC.source, 'g'), function(match, variableName, fallback) {
 		variablesUsedInValue.push(variableName);
-
-		//console.log(variableName, 'with', mimicChildOfList.length, '(mimic)parents');
-		//console.log('================================================');
 
 		// Loop through the list of declarations for that value and find the one that best matches
 		// By best match, we mean, the variable actually applies. Criteria:
@@ -66,7 +61,6 @@ var resolveValue = function(decl, map, /*optional*/mimicChildOf) {
 					// And if the current matching variable is already important, a new one to replace it has to be important
 					var isRoot = varDeclMapItem.parent.type === 'root' || (varDeclMapItem.parent.selectors.length === 1 && varDeclMapItem.parent.selectors[0] === ':root');
 					
-					//console.log('mpn', mimicParentNode.selector || mimicParentNode.type, varDeclMapItem.parent.selector);
 					if(
 						(
 							// If it is a root declared variable, not under a @rule
@@ -79,24 +73,8 @@ var resolveValue = function(decl, map, /*optional*/mimicChildOf) {
 						// And if the currently matched declaration is `!important`, it will take another `!important` to override it
 						(!(matchingVarDeclMapItem || {}).isImportant || varDeclMapItem.isImportant)
 					) {
-						/* * /
-						console.log(
-							'most matching now:',  varDeclMapItem.value, 
-							'\n\t', currentVarDeclDepth, matchingVarDeclDepth
-						);
-						/* */
-
 						matchingVarDeclMapItem = varDeclMapItem;
 						matchingVarDeclDepth = currentVarDeclDepth;
-					}
-					else {
-						/* * /
-						console.log(
-							'rejected because', varDeclMapItem.value,
-							'\n\t', (isRoot && !varDeclMapItem.isUnderAtRule), mimicParentNode.toString() === varDeclMapItem.parent.toString(),
-							'\n\t', currentVarDeclDepth, matchingVarDeclDepth
-						);
-						/* */
 					}
 				});
 
@@ -140,8 +118,11 @@ module.exports = postcss.plugin('postcss-css-variables', function (options) {
 	// Work with opts here
 
 	return function (css, result) {
-		//console.log('css', css);
 		// Transform CSS AST here
+
+		/* * /
+		try {
+		/* */
 
 		// List of nodes to add at the end
 		// We use this so we don't add to the tree as we are processing it (infinite loop)
@@ -194,146 +175,142 @@ module.exports = postcss.plugin('postcss-css-variables', function (options) {
 		}
 
 
-		try {
-			// Collect all of the variables defined
-			css.eachRule(function(rule) {
-				var numberOfStartingChildren = rule.nodes.length;
+		// Collect all of the variables defined
+		css.eachRule(function(rule) {
+			var numberOfStartingChildren = rule.nodes.length;
 
-				// only variables declared for `:root` are supported for now
-				//var isRoot = rule.selectors.length === 1 && rule.selectors[0] === ':root';
-				//if(isRoot && (rule.parent.type === 'root' || rule.parent.type === 'atrule')) {
-					rule.each(function(decl, index) {
-						var prop = decl.prop;
-						// If declaration is a variable
-						if(RE_VAR_PROP.test(prop)) {
-							var resolvedValue = logResolveValueResult(resolveValue(decl, map)).value;
-							if(resolvedValue !== undefined) {
-								map[prop] = (map[prop] || []).concat({
-									prop: prop,
-									value: resolvedValue,
-									isImportant: decl.important,
-									// variables inside root or @rules (eg. @media, @support)
-									parent: decl.parent,
-									isUnderAtRule: decl.parent.parent.type === 'atrule'
-								});
+			// Loop through all of the declarations and grab the variables and put them in the map
+			rule.eachDecl(function(decl, index) {
+				var prop = decl.prop;
+				// If declaration is a variable
+				if(RE_VAR_PROP.test(prop)) {
+					var resolvedValue = logResolveValueResult(resolveValue(decl, map)).value;
+					if(resolvedValue !== undefined) {
+						map[prop] = (map[prop] || []).concat({
+							prop: prop,
+							value: resolvedValue,
+							isImportant: decl.important,
+							// variables inside root or @rules (eg. @media, @support)
+							parent: decl.parent,
+							isUnderAtRule: decl.parent.parent.type === 'atrule'
+						});
+					}
+
+					// Remove the variable declaration because they are pretty much useless after we resolve them
+					if(!opts.preserve) {
+						decl.removeSelf();
+					}
+					// Or we can also just show the computed value used for that variable
+					else if(opts.preserve === 'computed') {
+						decl.value = resolvedValue;
+					}
+					// Otherwise just keep them as var declarations
+				}
+			});
+
+			// We don't want to mess up their code if they wrote a empty rule
+			// We add to the clean up list if we removed some variable declarations to make it become empty
+			if(numberOfStartingChildren > 0 && rule.nodes.length <= 0) {
+				nodesToRemoveAtEnd.push(rule);
+			}
+		});
+
+
+		// Resolve variables everywhere
+		css.eachDecl(function(decl) {
+			// Ignore any variable declarations that we may be preserving from earlier
+			// Don't worry, they are already processed
+			if(!RE_VAR_PROP.test(decl.prop)) {
+				var valueResults = logResolveValueResult(resolveValue(decl, map));
+
+				// Resolve the cascade
+				// Now find any @rule declarations that need to be added below each rule
+				valueResults.variablesUsed.forEach(function(variableUsedName) {
+					(map[variableUsedName] || []).forEach(function(varDeclMapItem) {
+						if(varDeclMapItem.isUnderAtRule) {
+							// Create the clean atRule for which we place the declaration under
+							var atRuleNode = varDeclMapItem.parent.parent.clone().removeAll();
+
+							var ruleClone = decl.parent.clone().removeAll();
+							var declClone = decl.clone();
+							declClone.value = logResolveValueResult(resolveValue(decl, map, [varDeclMapItem.parent])).value;
+
+							// Add the declaration to our new rule
+							ruleClone.append(declClone);
+							// Add the rule to the atRule
+							atRuleNode.append(ruleClone);
+
+
+							// Since that atRuleNode can be nested in other atRules, we need to make the appropriate structure
+							var parentAtRuleNode = atRuleNode;
+							var currentAtRuleNode = varDeclMapItem.parent.parent;
+							while(currentAtRuleNode.parent.type === 'atrule') {
+								// Create a new clean clone of that at rule to nest under
+								var newParentAtRuleNode = currentAtRuleNode.parent.clone().removeAll();
+
+								// Append the old parent
+								newParentAtRuleNode.append(parentAtRuleNode);
+								// Then set the new one as the current for next iteration
+								parentAtRuleNode = newParentAtRuleNode;
+
+								currentAtRuleNode = currentAtRuleNode.parent;
 							}
 
-							// Remove the variable declaration because they are pretty much useless after we resolve them
-							if(!opts.preserve) {
-								decl.removeSelf();
-							}
-							// Or we can also just show the computed value used for that variable
-							else if(opts.preserve === 'computed') {
-								decl.value = resolvedValue;
-							}
-							// Otherwise just keep them as var declarations
+							createNodeCallbackList.push(function() {
+								// Put the atRuleStructure after the declaration's rule
+								decl.parent.parent.insertAfter(decl.parent, parentAtRuleNode);
+							});
 						}
 					});
-
-					// We don't want to mess up their code if they wrote a empty rule
-					// We add to the clean up list if we removed some variable declarations to make it become empty
-					if(numberOfStartingChildren > 0 && rule.nodes.length <= 0) {
-						nodesToRemoveAtEnd.push(rule);
-					}
-				//}
-			});
+				});
 
 
-			// Resolve variables everywhere
-			css.eachDecl(function(decl) {
-				// Ignore any variable declarations that we may be preserving from earlier
-				// Don't worry, they are already processed
-				if(!RE_VAR_PROP.test(decl.prop)) {
-					var valueResults = logResolveValueResult(resolveValue(decl, map));
+				// If we are preserving var(...) usage and the value changed meaning it had some
+				if(opts.preserve === true && decl.value !== valueResults.value) {
+					createNodeCallbackList.push(function() {
+						decl.cloneAfter();
 
-					/* */
-					// Resolve the cascade
-					// Now find any @rule declarations that need to be added below each rule
-					valueResults.variablesUsed.forEach(function(variableUsedName) {
-						(map[variableUsedName] || []).forEach(function(varDeclMapItem) {
-							if(varDeclMapItem.isUnderAtRule) {
-								// Create the clean atRule for which we place the declaration under
-								var atRuleNode = varDeclMapItem.parent.parent.clone().removeAll();
-
-								var ruleClone = decl.parent.clone().removeAll();
-								var declClone = decl.clone();
-								declClone.value = logResolveValueResult(resolveValue(decl, map, [varDeclMapItem.parent])).value;
-
-								ruleClone.append(declClone);
-								atRuleNode.append(ruleClone);
-
-
-								// Since that atRuleNode can be nested in other atRules, we need to make the appropriate structure
-								var parentAtRuleNode = atRuleNode;
-								var currentAtRuleNode = varDeclMapItem.parent.parent;
-								while(currentAtRuleNode.parent.type === 'atrule') {
-									// Create a new clean clone of that at rule to nest under
-									var newParentAtRuleNode = currentAtRuleNode.parent.clone().removeAll();
-
-									// Append the old parent
-									newParentAtRuleNode.append(parentAtRuleNode);
-									// Then set the new one as the current for next iteration
-									parentAtRuleNode = newParentAtRuleNode;
-
-									currentAtRuleNode = currentAtRuleNode.parent;
-								}
-
-								createNodeCallbackList.push(function() {
-									// Put the atRuleStructure after the declaration's rule
-									decl.parent.parent.insertAfter(decl.parent, parentAtRuleNode);
-								});
-							}
-						});
-					});
-					/* */
-
-
-					// If we are preserving var(...) usage and the value changed meaning it had some
-					if(opts.preserve === true && decl.value !== valueResults.value) {
-						createNodeCallbackList.push(function() {
-							decl.cloneAfter();
-
-							// Set the new value after we are done dealing with @rule stuff
-							decl.value = valueResults.value;
-						});
-					}
-					else {
 						// Set the new value after we are done dealing with @rule stuff
 						decl.value = valueResults.value;
-					}
-					
+					});
 				}
-			});
-
-
-
-			// Add some nodes that we need to add
-			// We use this so we don't add to the tree as we are processing it (infinite loop)
-			createNodeCallbackList.forEach(function(cb) {
-				cb();
-			});
-
-			// Clean up any nodes we don't want anymore
-			nodesToRemoveAtEnd.forEach(function(currentChildToRemove) {
-				// If we removed all of the declarations in the rule(making it empty), then just remove it
-				var currentNodeToPossiblyCleanUp = currentChildToRemove;
-				while(currentNodeToPossiblyCleanUp && currentNodeToPossiblyCleanUp.nodes.length <= 0) {
-					var nodeToRemove = currentNodeToPossiblyCleanUp;
-					// Get a reference to it before we remove and lose reference to the child after removing it
-					currentNodeToPossiblyCleanUp = currentNodeToPossiblyCleanUp.parent;
-
-					nodeToRemove.removeSelf();
+				else {
+					// Set the new value after we are done dealing with @rule stuff
+					decl.value = valueResults.value;
 				}
-			});
+				
+			}
+		});
 
 
 
+		// Add some nodes that we need to add
+		// We use this so we don't add to the tree as we are processing it (infinite loop)
+		createNodeCallbackList.forEach(function(cb) {
+			cb();
+		});
+
+		// Clean up any nodes we don't want anymore
+		nodesToRemoveAtEnd.forEach(function(currentChildToRemove) {
+			// If we removed all of the declarations in the rule(making it empty), then just remove it
+			var currentNodeToPossiblyCleanUp = currentChildToRemove;
+			while(currentNodeToPossiblyCleanUp && currentNodeToPossiblyCleanUp.nodes.length <= 0) {
+				var nodeToRemove = currentNodeToPossiblyCleanUp;
+				// Get a reference to it before we remove and lose reference to the child after removing it
+				currentNodeToPossiblyCleanUp = currentNodeToPossiblyCleanUp.parent;
+
+				nodeToRemove.removeSelf();
+			}
+		});
+
+
+
+		/* * /
 		}
 		catch(e) {
 			console.log('e', e.message, e.stack);
 		}
-
-		//console.log('map', map);
+		/* */
 
 	};
 });
